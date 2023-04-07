666dc5d063576ca48d2acef57ded868e63f0cb5a
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-07 16:19:27 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -63,8 +63,6 @@ window.addEventListener("DOMContentLoaded", async () => {
     await loadMarkdown("about-content", `md/${contentId}/body.md`);
     await loadMarkdown("pagination-content", `md/${contentId}/pagination.md`);
   }
-
-  countWords();
 });
 
 function countWords() {
