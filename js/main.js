424272f6bcc43b3e39aa8db278636dfe29ca5102
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-07 15:45:09 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ async function loadMarkdown(containerId, filePath, isIntro) {
   const markdown = await response.text();
   const container = document.getElementById(containerId);
 
+  if (!markdown) {
+    container.classList.add('empty');
+    return;
+  }
+
   // Add content
   const contentElement = document.createElement("div");
   contentElement.innerHTML = marked(markdown);
@@ -21,16 +26,38 @@ async function loadMarkdown(containerId, filePath, isIntro) {
   }
 }
 
+async function getTotalReadingTime() {
+  const contentId = document.getElementById("content").dataset.content;
+  if (contentId === 'cover') {
+    let totalTime = 0;
+    const chapterPattern = /chapter-\d+/g;
+    const files = await fetch('md/');
+    const fileList = await files.text();
+    const chapters = fileList.match(chapterPattern);
+
+    for (const chapter of chapters) {
+      const response = await fetch(`md/${chapter}/intro.md`);
+      const introMd = await response.text();
+      const response2 = await fetch(`md/${chapter}/body.md`);
+      const bodyMd = await response2.text();
+      const words = (introMd + bodyMd).split(/\s+/g).length;
+      totalTime += Math.ceil(words / 200);
+    }
+    return totalTime;
+  }
+}
+
 window.addEventListener("DOMContentLoaded", async () => {
   await loadMarkdown("nav", "md/nav.md");
   await loadMarkdown("footer-content", "md/footer.md");
 
   const contentId = document.getElementById("content").dataset.content;
   if (contentId) {
-    await loadMarkdown("intro-wrapper", `md/${contentId}/intro.md`, true);
-    await loadMarkdown("about-content", `md/${contentId}/body.md`, false);
-    await loadMarkdown("pagination-content", `md/${contentId}/pagination.md`, false);
-    countWords();
+    const totalTime = await getTotalReadingTime();
+    const isCover = contentId === 'cover';
+    await loadMarkdown("intro-wrapper", `md/${contentId}/intro.md`, !isCover, isCover ? totalTime : null);
+    await loadMarkdown("about-content", `md/${contentId}/body.md`);
+    await loadMarkdown("pagination-content", `md/${contentId}/pagination.md`);
   }
 });
 
